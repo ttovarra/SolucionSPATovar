6d7e28d1621a808b2277a468d390cb16c1291928
ttovarra <ttovarra@gmail.com>
2023-10-20 20:47:56 -0500

    promer commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>SPA--Tovar SA de CV</title>
+<link rel="stylesheet" type="text/css" href="styles.css"><!--Hipervinculo interno para llamada de CSS-->
+<!--Hipervinculo externo para llamada de CSS-->
+<link href='https://fonts.googleapis.com/css?family=Stint+Ultra+Condensed&#39; rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,700&#39; rel='stylesheet' type='text/css'>        
+    </head>
+    <body>
+        <header class="header">
+            <h1><img src="images/logo.png"></h1>
+        </header>
+        <nav class="navbar">
+            <a href="">Inicio</a>
+            <a href="">Quienes somos</a>
+            <a href="">Localizacion</a>
+            <a href="">Tratamientos</a>
+            <a href="">Tarifas</a>
+        </nav>
+    <section class="content tratamientos">
+        <h2 class="title">Tratamientos</h2>
+        <p>Mejores pecios <br>Mejores promociones </p>
+
+        <ul class="list-tratamientos">
+            <li>
+                <figure>
+                    <img src="images/ico-facial.png">
+                    <figcaption>Faciales</figcaption>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <img src="images/ico-corporal.png">
+                    <figcaption>Corporales</figcaption>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <img src="images/ico-spa.png">
+                    <figcaption>Spa</figcaption>
+                </figure>
+            </li>
+        </ul>
+    </section>
+    
+<section class="content tarifas">
+        <article class="contain">
+            <h2 class="title">Tarifas</h2>
+            <p>Si quieres saber las tarifas de nuestros tratamientos no dudes en ponerte en contacto con nosotros.</p>
+            <p>Resolveremos cualquier duda que tengas.</p>
+
+            <a href="" class="btn">Contáctanos</a>
+        </article>
+</section>
+
+<section class="content localizacion">
+        <h2 class="title">Localización</h2>
+        <p>Lugar mas accesible<br>Te esperamos.</p>
+        <figure class="map"><img src="images/dummy-mapa.jpg" alt="" title=""></figure>
+ </section>   
+        
+    </body>
+</html>
